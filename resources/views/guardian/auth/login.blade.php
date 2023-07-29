4bb211ff77c1e6c7bad1d95364913175b9d1bb77
skyhackeR <jolayemidavid6@gmail.com>
2023-07-29 13:12:52 +0100

    Guardian authentication

--- resources/views/guardian/auth/login.blade.php
+++ resources/views/guardian/auth/login.blade.php
@@ -1,68 +1,63 @@
 @extends('guardian.layout.auth')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/guardian/login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember"> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ url('/guardian/password/reset') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+<div class="col-lg-8">
+    <div class="p-lg-5 p-4">
+        <div>
+            <h5 class="text-primary">Welcome Back !</h5>
+            <p class="text-muted">Sign in to continue to Guardian Portal.</p>
         </div>
+
+        <div class="mt-4">
+            <form method="POST" action="{{ url('/guardian/login') }}">
+                @csrf
+                <div class="mb-3" class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="text" class="form-control" name="email" id="email" placeholder="Enter email" value="{{ old('email') }}" autofocus>
+
+                    @if ($errors->has('email'))
+                        <br>
+                        <div class="alert alert-danger" role="alert">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </div>
+                    @endif
+                </div>
+
+
+
+                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }} mb-3">
+                    <div class="float-end">
+                        <a href="{{ url('/guardian/password/reset') }}" class="text-muted">Forgot password?</a>
+                    </div>
+                    <label class="form-label" for="password-input">Password</label>
+                    <div class="position-relative auth-pass-inputgroup mb-3">
+                        <input type="password" class="form-control pe-5 password-input" name="password" placeholder="Enter password" id="password-input">
+                        <button class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted password-addon" type="button" id="password-addon"><i class="ri-eye-fill align-middle"></i></button>
+                    </div>
+
+
+                    @if ($errors->has('password'))
+                        <br>
+                        <div class="alert alert-danger" role="alert">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </div>
+                    @endif
+                </div>
+
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="" id="auth-remember-check">
+                    <label class="form-check-label" for="auth-remember-check">Remember me</label>
+                </div>
+
+                <div class="mt-4">
+                    <button class="btn btn-success w-100" type="submit">Sign In</button>
+                </div>
+            </form>
+        </div>
+
+        <!-- <div class="mt-5 text-center">
+            <p class="mb-0">Don't have an account ? <a href="{{url('/guardian/register')}}" class="fw-semibold text-primary text-decoration-underline"> Signup</a> </p>
+        </div> -->
     </div>
 </div>
 @endsection
--- resources/views/guardian/auth/passwords/email.blade.php
+++ resources/views/guardian/auth/passwords/email.blade.php
@@ -2,45 +2,40 @@
 
 <!-- Main Content -->
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+
+<div class="col-lg-8">
+    <div class="p-lg-5 p-4">
+        <div>
+            <h5 class="text-primary">Welcome Back !</h5>
+            <p class="text-muted">Reset Portal</p>
+        </div>
+
+        <div class="mt-4">
+            <form method="POST" action="{{ url('/guardian/password/email') }}">
+                @csrf
+
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+
+                <div class="mb-3" class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="text" class="form-control" name="email" id="email" placeholder="Enter email" value="{{ old('email') }}" autofocus>
+
+                    @if ($errors->has('email'))
+                        <br>
+                        <div class="alert alert-danger" role="alert">
+                            <strong>{{ $errors->first('email') }}</strong>
                         </div>
                     @endif
-
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/guardian/password/email') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
                 </div>
-            </div>
+
+                <div class="mt-4">
+                    <button class="btn btn-success w-100" type="submit">Send Password Reset Link</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
