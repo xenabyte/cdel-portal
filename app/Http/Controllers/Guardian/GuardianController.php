add81784d667396e0eab7bf7e0e053d64fbab9fd
skyhackeR <jolayemidavid6@gmail.com>
2023-07-31 16:35:01 +0100

    guardian dashboard

---
+++ app/Http/Controllers/Guardian/GuardianController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Guardian;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests;
+use Illuminate\Support\Facades\Validator;
+
+use App\Models\Guardian;
+
+use SweetAlert;
+use Mail;
+use Alert;
+use Log;
+use Carbon\Carbon;
+
+class GuardianController extends Controller
+{
+    //
+
+    public function index(Request $request){
+
+        return view('guardian.home');
+    }
+}
--- resources/views/guardian/home.blade.php
+++ resources/views/guardian/home.blade.php
@@ -1,17 +1,5 @@
-@extends('guardian.layout.auth')
+@extends('guardian.layout.dashboard')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
-                    You are logged in as Guardian!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
