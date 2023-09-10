43c535f8e85bc91ad763362dc58cdd90eb1b6c11
xenabyte <iconcept573@gmail.com>
2023-09-10 15:59:13 +0100

    sentry installation

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -32,10 +32,11 @@ class Handler extends ExceptionHandler
      *
      * @return void
      */
-    public function register()
-    {
+    public function register() {
         $this->reportable(function (Throwable $e) {
-            //
+          if (app()->bound('sentry')) {
+            app('sentry')->captureException($e);
+          }
         });
-    }
+      }
 }
