9f6b399573e9044eb3d76480d69b77998f9406cf
xenabyte <iconcept573@gmail.com>
2023-09-15 13:29:49 +0100

    remove plugin.js

--- resources/views/guardian/layout/auth.blade.php
+++ resources/views/guardian/layout/auth.blade.php
@@ -116,7 +116,8 @@
     <script src="{{asset('assets/libs/node-waves/waves.min.js')}}"></script>
     <script src="{{asset('assets/libs/feather-icons/feather.min.js')}}"></script>
     <script src="{{asset('assets/js/pages/plugins/lord-icon-2.1.0.js')}}"></script>
-{{-- <script src="{{asset('assets/js/plugins.js')}}"></script> --}}
+    {{-- <script src="{{asset('assets/js/plugins.js')}}"></script> --}}
+
     <!-- password-addon init -->
     <script src="{{asset('assets/js/pages/password-addon.init.js')}}"></script>
 </body>
