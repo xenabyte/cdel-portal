877e8dc286238877dc60b7e91b3f59ec34d00457
xenabyte <iconcept573@gmail.com>
2023-09-17 09:41:08 +0100

    bursary

--- app/Http/Controllers/Admin/PaymentController.php
+++ app/Http/Controllers/Admin/PaymentController.php
@@ -192,7 +192,7 @@ class PaymentController extends Controller
         if($userType == 'applicant') {
             $applicant = Applicant::with('programme', 'student')->where('id', $request->student_id)->first();
             $applicantId = $applicant->id;
-            if($empty($applicant->student)){
+            if(empty($applicant->student)){
                 $studentId = $applicant->student->id;
             }
         }
@@ -355,7 +355,7 @@ class PaymentController extends Controller
         if(!empty($request->user_id)){
             $applicant = Applicant::with('programme', 'student')->where('id', $request->user_id)->first();
             $applicantId = $applicant->id;
-            if($empty($applicant->student)){
+            if(empty($applicant->student)){
                 $studentId = $applicant->student->id;
             }
         }
