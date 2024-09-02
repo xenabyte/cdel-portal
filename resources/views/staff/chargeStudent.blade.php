f03d8b9c94f8e686af1305c77fba0081c56cd44c
KoderiaNG <iconcept573@gmail.com>
2024-09-02 14:10:30 +0100

    edit transaction as staff

--- resources/views/staff/chargeStudent.blade.php
+++ resources/views/staff/chargeStudent.blade.php
@@ -464,6 +464,14 @@
                                             <button type="submit" id="submit-button" class="btn btn-primary"><i class="mdi mdi-printer"></i></button>
                                         </form>
                                         @endif
+
+                                        <form action="{{ url('/staff/getStudentPayment') }}" method="post" enctype="multipart/form-data">
+                                            @csrf
+                                            <input name="payment_id" type="hidden" value="{{$filteredTransaction['id']}}">
+                                            <input name="student_id" type="hidden" value="{{$student->id}}">
+                                            <input name="session" type="hidden" value="{{ $filteredTransaction['session'] }}">
+                                            <button type="submit" id="submit-button" class="btn btn-info my-1"><i class="mdi mdi-eye"></i></button>
+                                        </form>
                                     </td>
                                 </tr>
                             @endforeach
