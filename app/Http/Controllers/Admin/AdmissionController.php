bf24affa14ae09a7d132dfa275c4f701a7506526
KoderiaNg <iconcept573@gmail.com>
2025-05-06 10:20:15 +0100

    fix admission

--- app/Http/Controllers/Admin/AdmissionController.php
+++ app/Http/Controllers/Admin/AdmissionController.php
@@ -260,7 +260,6 @@ class AdmissionController extends Controller
         $students = Student::with('applicant', 'programme')
             ->where('academic_session', $admissionSession)
             ->where('programme_category_id', $programmeCategoryId)
-            ->where('matric_number', null)
             ->whereHas('applicant', function ($query) use ($admissionSession) {
                 $query->where('academic_session', $admissionSession);
             })
