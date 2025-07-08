ad81d6ce9c5b80a0db43203d322920851b2614b5
KoderiaNg <iconcept573@gmail.com>
2025-07-08 17:11:31 +0100

    unset staff

--- app/Http/Controllers/Staff/StaffController.php
+++ app/Http/Controllers/Staff/StaffController.php
@@ -743,10 +743,10 @@ class StaffController extends Controller
     }
 
     public function unsetStaff(Request $request){
-        $validator = Validator::make($request->all(), [
-            'course_id' => $request->course_id,
-            'staff_id' => $request->staff_id,
-            'programme_category_id' => $request->programme_category_id,
+       $validator = Validator::make($request->all(), [
+            'staff_id' => 'required',
+            'course_id' => 'required',
+            'programme_category_id' => 'required'
         ]);
 
         if($validator->fails()) {
@@ -754,13 +754,13 @@ class StaffController extends Controller
             return redirect()->back();
         }
 
-        $programmeCategory = ProgrammeCategory::with('academicSessionSetting')->where('category', $request->programmeCategory)->first();
-        $academicSession = $programmeCategory->academicSessionSetting->academic_session ?? null;
-
-        if (!$academicSession) {
+        $programmeCategory = ProgrammeCategory::with('academicSessionSetting')->where('id', $request->programme_category_id)->first();
+        if (empty($programmeCategory->academicSessionSetting)) {
             alert()->error('Oops!', 'Session setting for programme category not found.')->persistent('Close');
             return redirect()->back();
         }
+        $academicSession = $programmeCategory->academicSessionSetting->academic_session;
+
 
         $unset = CourseManagement::where([
             'course_id' => $request->course_id,
